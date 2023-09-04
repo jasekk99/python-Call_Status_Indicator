ce7134a8d77e9c7c5faf2cefdfe12ae63eb1d1ca
Jack Green <jack.green@bbzbl-it.ch>
2023-09-04 15:57:53 +0200

    Added more status'
    Added "Busy" and "Available". If statement for deciding which colours to set the LEDs was changed to a match(switch) statement

--- main/setLEDs.py
+++ main/setLEDs.py
@@ -5,6 +5,14 @@ from blinkstick import blinkstick
 #Find the first BlinkStick
 bstick = blinkstick.find_first()
 
-if current_status == 'OnThePhone':
-    #Use R, G and B channels to control single RGB LED
-    bstick.set_mode(red=255, green=0, blue=0)
+
+#Use R, G and B channels to control single RGB LED
+match current_status:
+    case 'OnThePhone': 
+        bstick.set_mode(red=255, green=0, blue=0)       #Set LEDs Red
+    case 'Busy':
+        bstick.set_mode(red=235, green=168, blue=52)    #Set LEDs Orange
+    case 'Available':
+        bstick.set_mode(red=0, green=255, blue=0)       #Set LEDs Green
+    case _:
+        bstick.turn_off()
