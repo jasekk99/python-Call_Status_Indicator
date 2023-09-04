beee616418dd0088d41673ad057fbf1057fc802d
Jack Green <jack.green@bbzbl-it.ch>
2023-09-04 11:56:56 +0200

    Create setLEDs.py
    monitorStatus.py calls this file for all LED operations

---
+++ main/setLEDs.py
@@ -0,0 +1,10 @@
+from __main__ import *
+import time
+from blinkstick import blinkstick
+
+#Find the first BlinkStick
+bstick = blinkstick.find_first()
+
+if current_status == 'OnThePhone':
+    #Use R, G and B channels to control single RGB LED
+    bstick.set_mode(red=255, green=255, blue=255)
