7e935d50c77a21ea562a9224f96518483c3c15d2
Jack Green <jack.green@bbzbl-it.ch>
2023-09-15 11:43:04 +0200

    File structure + Use all LEDs
    Changed file structure, so now all Python files are in the same directory.
    
    Now using all LEDs on the strip.
    
    Added "current_status" case 'DoNotDisturb' which activates Red LEDs.
    Changed the color for case 'Busy' to yellow.
    
    Added keyboard interrupt catch to turn off the LEDs.

---
+++ blinkstick/monitorStatus.py
@@ -0,0 +1,51 @@
+import time
+import os
+import sys
+from blinkstick import blinkstick
+
+substring_StatusIndicator_Added = "StatusIndicatorStateService: Added"
+separatorLeft = ": Added "
+separatorRight = " ("
+
+def follow(thefile):
+    #generator function that yields new lines in a file
+    # seek the end of the file
+    thefile.seek(0, os.SEEK_END)
+    
+    # start infinite loop
+    while True:
+        try:
+            # read last line of file
+            line = thefile.readline()        # sleep if file hasn't been updated
+            if not line:
+                time.sleep(0.1)
+                continue
+
+            yield line
+        except KeyboardInterrupt:
+            bstick = blinkstick.find_first()
+            def allLEDs(colour):
+                for i in range(7):
+                    bstick.set_color(index=i, name=colour)
+                    time.sleep(0.1)
+            allLEDs('black')
+            print("Excited through keyboard interrupt")
+            sys.exit()
+
+
+
+if __name__ == '__main__':
+    path_to_file = os.path.expanduser('~')+r"\AppData\Roaming\Microsoft\Teams\logs.txt"
+    logfile = open(path_to_file, "r")
+    print(path_to_file)
+    loglines = follow(logfile)    # iterate over the generator
+
+    for line in loglines:
+        if substring_StatusIndicator_Added in line:
+            stripped = line.split(separatorLeft,1)
+            final_stripped = stripped[1].split(separatorRight,1)
+            current_status = final_stripped[0]
+            print(current_status)
+            with open(r"blinkstick\setLEDs.py") as f:
+                exec(f.read())
+        
---
+++ blinkstick/setLEDs.py
@@ -0,0 +1,61 @@
+from __main__ import *
+import time
+from blinkstick import blinkstick
+
+
+error = False
+
+#Loops while error=True (until Blinkstick is recognized) then continues and sets error=False
+def wait_for_Blinkstick(error):
+    while error == True:
+        try:
+            blinkstick.find_first()
+        except:
+            error = True
+            time.sleep(1)
+    error = False
+
+#Find the first BlinkStick
+bstick = blinkstick.find_first()
+"""
+try:
+    bstick = blinkstick.find_first()
+    print('blinkstick.find_first(): '+bstick)
+    print('bstick found: '+print(bstick))
+except:
+    #BlinkStick not found or cannot be reached; error=True
+    error = True
+    print('No BlinkStick found (Check if the device is Plugged in via USB)')
+    #invoke loop (Wait for recognition)
+    wait_for_Blinkstick(error)
+"""
+
+
+print('Error: '+str(error))
+
+
+
+def allLEDs(colour):
+    for i in range(7):
+        bstick.set_color(index=i, name=colour)
+        time.sleep(0.1)
+
+
+if error == False:
+    #Use R, G and B channels to control single RGB LED
+    match current_status:
+        case 'OnThePhone': 
+            allLEDs('red')
+            #bstick.set_color(red=255, green=0, blue=0, name=None, hex=None)       #Set LEDs Red
+        case 'DoNotDisturb':
+            allLEDs('red')
+            #bstick.set_color(red=255, green=0, blue=0, name=None, hex=None)
+        case 'Busy':
+            allLEDs('yellow')
+            #bstick.set_color(red=255, green=255, blue=0, name=None, hex=None)    #Set LEDs Yellow
+        case 'Available':
+            allLEDs('black')
+            #bstick.turn_off()      #Turn LEDs off
+        case _:
+            allLEDs('black')
+            #bstick.turn_off()
